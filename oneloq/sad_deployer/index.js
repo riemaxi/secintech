b0161c4107ed9b4cc294c276aa0fe60c3ef6ab5e
samuel <ferraxi@gmail.com>
2020-11-03 20:15:39 +0100

    deployers

--- oneloq/sad_deployer/index.js
+++ oneloq/sad_deployer/index.js
@@ -1,7 +1,7 @@
 const app = require('express')()
-const port = 2000
+const port = 1000
 
 //session
 app.get('/check', (req, res) => res.json( {check : { responde: '0'}} ))
 
-app.listen(port, () => console.log(`OLS session listening at port ${port}`) )
+app.listen(port, () => console.log(`SAD session listening at port ${port}`) )
