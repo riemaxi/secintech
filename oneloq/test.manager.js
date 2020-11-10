6d04f3b8a25869d33ce7aacc4c93ea059cfaf644
samuel <ferraxi@gmail.com>
2020-11-11 00:11:49 +0100

    collection empty

--- oneloq/test.manager.js
+++ oneloq/test.manager.js
@@ -7,13 +7,11 @@ const config = {
 
 let manager = require('./manager').instance(config)
 
+//let owner = 'NOTTHERE202011178891AAF'
 let owner = '202011178891AAF'
-let start = '2020-11-01T00:00:00'
-//let end = '2020-11-02T00:00:00'
-let end = '2021-11-01T00:00:00'
 let type = 0
 let data = 'AAAAXXXFF'
 
 let port = 30001
 
-manager.checkAccess(port, owner, start, end, type, data, (res) => console.log('access: ' + res))
+manager.checkAccess(port, owner,type, data, (res) => console.log('access: ' + res))
