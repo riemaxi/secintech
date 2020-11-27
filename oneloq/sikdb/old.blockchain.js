ccc5fe251029f5fe44a80549fadc305c85b31bee
samuel <ferraxi@gmail.com>
2020-11-27 04:37:41 +0100

    memory

--- oneloq/sikdb/old.blockchain.js
+++ oneloq/sikdb/old.blockchain.js
@@ -9,8 +9,17 @@ class Blockchain{
 	}
 
 	initialize(){
-		this.blocks = ['']
-		this.items = []
+	    this.sql.dropTables(['block','item'])
+
+	   this.sql.createTable(
+		'block',
+		`hash varchar(${this.hashSize})`)
+
+	    this.sql.createTable(
+		'item',
+		'time bigint,block int, type int, contract varchar(256), sender varchar(256), requester varchar(256), recipient varchar(256),  data text')
+
+            this.sql.insert('block','hash',"''")
 	}
 
 	sha256(data){
@@ -18,39 +27,18 @@ class Blockchain{
 	}
 
 	blockchainHash(block, data, handle){
-		handle( this.sha256( this.blocks[block] + data ) )
-
-		//this.sql.collection(`select hash from block where rowid = ${block}`, (item) => handle(this.sha256(item.hash + data)))
+		this.sql.collection(`select hash from block where rowid = ${block}`, (item) => handle(this.sha256(item.hash + data)))
 	}
 
 	blockHash(block, handle){
 		var data = ''
-		for(var i=0; i<this.items.length; i++)
-			if (this.items[i].block == block)
-				data += this.items[i].data
-		handle(this.sha256(data))
-
-		/*var data = ''
 		this.sql.collectionToClosure(`select data from item where block = ${block}`,
 			(item) => data += item.data,() => handle(this.sha256(data) ),
-			()=>{} )*/
+			()=>{} )
 	}
 
 	addItem(block,type, contract, sender, requester, recipient, data, handle){
-		this.items.push({ 
-				time: new Date().getTime() , 
-				block: block,
-				type : type,
-				contract: contract,
-				sender: sender,
-				requester: requester,
-				recipient: recipient,
-				data: data })
-
-		this.blockHash(block, (hash) => this.blocks[block] = hash)
-		handle(0)
-
-		/*console.log('add item')
+		console.log('add item')
 
 		let time = new Date().getTime()
 
@@ -63,7 +51,7 @@ class Blockchain{
 					console.log(`update block set hash = '${hash}' where rowid = ${block}`)
 					this.sql.exec(`update block set hash = '${hash}' where rowid = ${block}`, (err) => handle(err))
 				})
-			})*/
+			})
 	}
 
 	pack(type, contract, sender, requester, recipient, data){
@@ -72,84 +60,45 @@ class Blockchain{
 
 	addBlockAndItem(prevBlock, type, contract, sender, requester, recipient, data, handle){
 		this.blockchainHash(prevBlock, this.pack(type, contract, sender, requester, recipient, data), (hash) => {
-			this.blocks.push(hash)
-			this.addItem(prevBlock + 1, type, contract, sender, requester, recipient, data, handle)
-		} )
-
-		/*this.blockchainHash(prevBlock, this.pack(type, contract, sender, requester, recipient, data), (hash) => {
 			this.sql.insert('block','hash',`'${hash}'`, handle('block and item added'), () =>{
 				this.addItem(prevBlock + 1, type, contract, sender, requester, recipient, data, handle)
 			})
-		} )*/
+		} )
 
 	}
 
 	collection(limit, handle){
-		for(var i=0; i<this.items.length; i++)
-			handle(this.items[i])
-
-		//this.sql.collection(`select * from item order by rowid desc limit ${limit}`, handle, ()=>{})
+		this.sql.collection(`select * from item order by rowid desc limit ${limit}`, handle, ()=>{})
 	}
 
 
 	collectionToClosure(limit, handle, end, empty){
-		if (this.items.length == 0)
-			empty()
-		else{
-			for(var i=0; i<this.items.length; i++)
-				handle(this.items[i])
-			end()
-		}
-
-		//this.sql.collectionToClosure(`select * from item order by rowid desc limit ${limit}`, handle, end, empty)
+		this.sql.collectionToClosure(`select * from item order by rowid desc limit ${limit}`, handle, end, empty)
 	}
 
 	lastHash(handle){
-		handle(this.blocks[this.blocks.length - 1])
-
-		//this.sql.collection('select max(rowid), hash from block', (item) => handle(item.hash))
+		this.sql.collection('select max(rowid), hash from block', (item) => handle(item.hash))
 	}
 
 	checkConsistency(handle){
-		for(var i=0; i<this.block.length; i++)
-			this.blockHash(i, (calchash) => {
-				handle(i, this.blocks[i] == calchash)
-			})
-
-		/*this.sql.collection('select rowid as id, hash from block order by id',
+		this.sql.collection('select rowid as id, hash from block order by id',
 			(item) => {
 				this.blockHash( item.id, (calchash) => {
 					handle(item.id, item.hash == calchash)
 				})
-		})*/
-	}
-
-	reminding(){
-		var c = 0;
-		let block = this.blocks.length -1
-		for(var i=0; i<this.items.length; i++)
-			if (this.items[i].block == block)
-				c++
-		return c
+		})
 	}
 
 	mine(type, contract, sender, requester, recipient, data , handle){
-		console.log('mining ...	')
-		let lastblock = this.blocks.length - 1
-		if (this.reminding() < this.capacity)
-			this.addItem(lastblock, type, contract, sender, requester, recipient, data , handle)
-				else
-			this.addBlockAndItem(lastblock, type, contract, sender, requester, recipient, data, handle)
-
-
-		/*this.sql.collection('select max(rowid) id from block', (blockitem) => {
+		this.sql.collection('select max(rowid) id from block', (blockitem) => {
 			this.sql.collection(`select count(*) size from item where block = ${blockitem.id}`, (item) => {
 				if (item.size < this.capacity)
 					this.addItem(blockitem.id, type, contract, sender, requester, recipient, data , handle)
+
 				else
 					this.addBlockAndItem(blockitem.id, type, contract, sender, requester, recipient, data, handle)
 			})
-		})*/
+		})
 	}
 }
 
--- oneloq/sikdb/old.dbmanager.js
+++ oneloq/sikdb/old.dbmanager.js
@@ -15,21 +15,16 @@ class DBManager extends SQLManager{
   }
 
   populateUsers(){
-	this.users.push({ user: 'u', password : 'p'})
-     /*console.log('popUser:' + this.superuser)
+     console.log('popUser:' + this.superuser)
     this.insert('access','user, password',`'${this.superuser}','${this.superpassword}'`)
 
     this.collection('select * from access', (item) => {
       console.log(item)
-    })*/
+    })
   }
 
   initializeData(){
-    this.keys = []
-    this.users = []
-    this.contracts =  []
-
-    /*this.dropTables(['key','access','contract'])
+    this.dropTables(['key','access','contract'])
 
     this.createTable(
       'access',
@@ -42,51 +37,23 @@ class DBManager extends SQLManager{
     this.createCleanTable(
       'contract',
       'id varchar(256) primary key,signed unsigned big int, start unsigned big int, end unsigned big int,   parta varchar(256), partb varchar(256), type varchar(256), price varchar(256), parameters text, procedure text'
-    )*/
+    )
 
     this.populateUsers()
   }
 
    reset(res){
-	//this.transaction(['delete from block','delete from item'], (result) => res.json(result))
-	this.bc.initialize()
+	this.transaction(['delete from block','delete from item'], (result) => res.json(result))
   }
 
 	accessLookup(params, res){
 		let user = params.user
 		let password = params.password
-		let hit = this.users.find( e => e.user == user && e.password == password)
-		res.json({ response: hit != null })
-		/*let query  = `select count(*) size from access where user = '${user}' and password = '${password}'`
-		this.collection( query, (item) => res.json({ response: item.size > 0 }) )*/
+		let query  = `select count(*) size from access where user = '${user}' and password = '${password}'`
+		this.collection( query, (item) => res.json({ response: item.size > 0 }) )
 	}
 
 	keyAdd(params, res){
-		let owner = params.owner
-		let start = params.start
-		let end = params.end
-		let type = params.type
-		let data = params.data
-		let status = constant.key.status.INACTIVE
-
-		this.keys.push({
-			owner : owner,
-			start : start,
-			end : end,
-			type : type,
-			data : params.data,
-			status : constant.key.status.INACTIVE
-		})
-
-		this.mine(
-			constant.tx.type.KEYADD,
-			params,
-			`${owner}|${start}|${end}|${type}|${data}|${status}`,
-			() => res.json({ response: constant.key.response.status.OK})
-		)
-
-
-		/*
 		let owner = params.owner
 		let start = params.start
 		let end = params.end
@@ -102,33 +69,42 @@ class DBManager extends SQLManager{
 					`${owner}|${start}|${end}|${type}|${data}|${status}`,
 					() => res.json({ response: constant.key.response.status.OK})
 				)
-			})*/
+			})
+	}
+
+	oldkeyUpdateStatus(params, res){
+		let owner = params.owner
+		let deactivated = constant.key.status.DEACTIVATED
+
+		this.collectionToClosure(`select * from key where owner = '${owner}' and status > ${deactivated}`, (item) => {
+
+			let status = params.value
+			this.exec(`update key set status = ${status} where owner = '${owner}' and status > ${deactivated}'`,
+				(err)=>{
+					let start = item.start
+					let end = item.end
+					let type = item.type
+					let data = item.data
+
+					this.mine(
+						constant.tx.type.KEYUPDATE,
+						params,
+						`${owner}|${start}|${end}|${type}|${data}|${status}`,
+						() => res.json({ response: constant.key.response.status.OK})
+					)
+				})
+		},
+		() => {},
+	        ()=> res.json({ response: constant.key.response.status.NOTFOUND} ) )
+
 	}
 
 	keyUpdateStatus(params, res){
 		let owner = params.owner
 		let deactivated = constant.key.status.DEACTIVATED
+
 		let status = params.value
-
-		var item = this.keys.find(e => e.owner == owner && e.status != deactivated)
-		if (item != null){
-			item.status = status
-
-			let start = item.start
-			let end = item.end
-			let type = item.type
-			let data = item.data
-
-			this.mine(
-				constant.tx.type.KEYUPDATE,
-				params,
-				`${owner}|${start}|${end}|${type}|${data}|${item.status}`,
-				() => res.json({ response: constant.key.response.status.OK})
-			)
-		}else
-			res.json({ response: constant.key.response.status.NOTFOUND})
-
-		/*console.log(`keyUpdateStatus: update key set status = ${status} where owner = '${owner}' and status > ${deactivated}'`)
+		console.log(`keyUpdateStatus: update key set status = ${status} where owner = '${owner}' and status > ${deactivated}'`)
 
 		this.exec(`update key set status = ${status} where owner = '${owner}' and status > ${deactivated}'`,
 			(err)=>{
@@ -149,7 +125,7 @@ class DBManager extends SQLManager{
 				() => {},
 			        ()=> {} )
 
-			})*/
+			})
 
 	}
 
@@ -160,17 +136,7 @@ class DBManager extends SQLManager{
 		let data = params.data
 		let time = params.time
 		let active = constant.key.status.ACTIVE
-
-		let item = this.keys.find(e => e.start <= time && time <= e.end && e.owner == owner && e.type == type  && e.data == data && e.status == active)
-
-		this.mine(
-			constant.tx.type.KEYLOOKUP,
-			params,
-			`${owner}|${time}|${type}|${data}`,
-			()=> res.json({response: item != null })
-		)
-
-		/*let query = `select count(*) size from key where ${time} between start and end and owner = '${owner}' and type = ${type} and data = '${data} and status = ${active}'`
+		let query = `select count(*) size from key where ${time} between start and end and owner = '${owner}' and type = ${type} and data = '${data} and status = ${active}'`
 		this.collection(query, (item) => {
 			this.mine(
 				constant.tx.type.KEYLOOKUP,
@@ -178,7 +144,7 @@ class DBManager extends SQLManager{
 				`${owner}|${time}|${type}|${data}`,
 				()=> res.json({response: item.size > 0 })
 			)
-		})*/
+		})
 
 	}
 
